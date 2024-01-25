14f0c189a6135387c72d4b2c413c670666c3a5f2
Piyush7982 <ajain9213742679@gmail.com>
2024-01-25 16:08:27 +0530

    test2

--- src/helper/axios.instance.js
+++ src/helper/axios.instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:4000";
-// const BACKEND_URL = "https://task-manager-49fq.onrender.com";
+// const BACKEND_URL = "http://localhost:4000";
+const BACKEND_URL = "https://task-manager-49fq.onrender.com";
 const axiosInstance = axios.create({
   baseURL: `${BACKEND_URL}/api/v1`,
   withCredentials: true,
